71a4a619313c98b6ec63c064aae4072f8adb0d07
strbytes <strbytes@gmail.com>
2023-01-03 16:38:18 -0500

    Update README.md and run Black formatter

--- eggbot.py
+++ eggbot.py
@@ -3,65 +3,71 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-bench_regex_m = re.compile(r'\W(dad|brother|guy|man|boyfriend)\W') # removed ^.*? from beginning and
-bench_regex_f = re.compile(r'\W(mom|sister|girl|woman|girlfriend)\W') # added \Ws at beginning and end
-bench_regex_n = re.compile(r'\W(friend|boss|roommate|girlfriends|boyfriends)\W')
-word_regex = re.compile(r'\w+')
-insult_trigger = re.compile(r'\!insult$')
+bench_regex_m = re.compile(
+    r"\W(dad|brother|guy|man|boyfriend)\W"
+)  # removed ^.*? from beginning and
+bench_regex_f = re.compile(
+    r"\W(mom|sister|girl|woman|girlfriend)\W"
+)  # added \Ws at beginning and end
+bench_regex_n = re.compile(r"\W(friend|boss|roommate|girlfriends|boyfriends)\W")
+word_regex = re.compile(r"\w+")
+insult_trigger = re.compile(r"\!insult$")
 
-with open('eggfacts.txt') as f:
-    egg_facts = [fact for fact in f.read().strip().split('\n')]
-with open('wizards.txt') as f:
-    wizards = [wizard for wizard in f.read().split('\n\n')]
+with open("eggfacts.txt") as f:
+    egg_facts = [fact for fact in f.read().strip().split("\n")]
+with open("wizards.txt") as f:
+    wizards = [wizard for wizard in f.read().split("\n\n")]
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        print("Logged on as {0}!".format(self.user))
 
     async def on_message(self, message):
         if message.author == client.user:
             return
-        if 'egg' in message.content.lower():
-            print('Egg reaction!')
-            await message.add_reaction('🥚')
+        if "egg" in message.content.lower():
+            print("Egg reaction!")
+            await message.add_reaction("🥚")
         # None of the bench responses have triggered in a while, I think the
         # regexes might be broken
-        if bench_regex_m.match(message.content) and random.random() > .75:
-            print('Bench response!')
-            await message.reply('How much does he bench?')
-        if bench_regex_f.match(message.content) and random.random() > .75:
-            print('Bench response!')
-            await message.reply('How much does she bench?')
-        if bench_regex_n.match(message.content) and random.random() > .75:
-            print('Bench response!')
-            await message.reply('How much do they bench?')
-        if message.content == '!facts':
-            print('Egg facts!')
+        if bench_regex_m.match(message.content) and random.random() > 0.75:
+            print("Bench response!")
+            await message.reply("How much does he bench?")
+        if bench_regex_f.match(message.content) and random.random() > 0.75:
+            print("Bench response!")
+            await message.reply("How much does she bench?")
+        if bench_regex_n.match(message.content) and random.random() > 0.75:
+            print("Bench response!")
+            await message.reply("How much do they bench?")
+        if message.content == "!facts":
+            print("Egg facts!")
             await message.reply(random.choice(egg_facts))
-        if message.content == '!wizard':
-            print('Wizard!')
-            await message.reply('```' + random.choice(wizards) + '```')
-        # pick out the longest word from a message on a .01 chance and then say "you're a [word]"
-        if random.random() < .005 or insult_trigger.findall(message.content):
+        if message.content == "!wizard":
+            print("Wizard!")
+            await message.reply("```" + random.choice(wizards) + "```")
+        # pick out the longest word from a message on a .01 chance and then say
+        # "you're a [word]"
+        if random.random() < 0.005 or insult_trigger.findall(message.content):
             words = word_regex.findall(message.content)
-            longest = ''
+            longest = ""
             for word in words:
                 if len(word) > len(longest):
                     longest = word
             print("You're a word!")
-            if longest[0].lower() in 'aeiou':
+            if longest[0].lower() in "aeiou":
                 await message.reply(f"You're an {longest.lower()}!")
             else:
                 await message.reply(f"You're a {longest.lower()}!")
-        if message.content[:3] == '!kg':
+        if message.content[:3] == "!kg":
             lb = message.content[4:]
             kg = round(float(lb) / 2.2046226218, 2)
-            await message.reply(f'{lb} pounds is equal to {kg} kilograms')
-        if message.content[:3] == '!lb':
+            await message.reply(f"{lb} pounds is equal to {kg} kilograms")
+        if message.content[:3] == "!lb":
             kg = message.content[4:]
             lb = round(float(kg) * 2.2046226218, 2)
-            await message.reply(f'{kg} kilograms is equal to {lb} pounds')
+            await message.reply(f"{kg} kilograms is equal to {lb} pounds")
 
 
 client = MyClient()
-client.run(os.environ.get('DISCORD_API_KEY'))
+client.run(os.environ.get("DISCORD_API_KEY"))
